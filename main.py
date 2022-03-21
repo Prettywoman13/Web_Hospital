86c5acfe53645d0f8cbe306e3f15f178c9d65cfc
Sla1merAPK <slameryt4@gmail.com>
2022-03-21 15:04:45 +0200

    Registration page
    Added registration page
    Remark login page

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 from forms.login import LoginForm
+from forms.registration import RegistraionForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -14,6 +15,12 @@ def main():
 def login():
     form = LoginForm()
     return render_template("HTML/login.html", form=form)
+
+
+@app.route("/registration")
+def registration():
+    form = RegistraionForm()
+    return render_template("HTML/registration.html", form=form)
 
 
 if __name__ == '__main__':
