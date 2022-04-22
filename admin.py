fb137fb46b5c9dd70559f0619d6abc660c716cb4
Sla1merAPK <slameryt4@gmail.com>
2022-04-22 18:48:24 +0200

    QSDQW

--- admin.py
+++ admin.py
@@ -199,6 +199,11 @@ def add_schedule():
             return render_template('doc_schedule.html', doctors_data=doctor_list, form=form,
                                    message='Дата не корректа, вы не можете добавить талоны в прошлое',
                                    is_auth=current_user.is_authenticated)
+        if datetime.datetime.strptime(request.form['date'], '%Y-%m-%d') == '':
+            return render_template('doc_schedule.html', doctors_data=doctor_list, form=form,
+                                   message='Вы не выбрали дату',
+                                   is_auth=current_user.is_authenticated)
+
         tickets = get_schedule_list(
             [],
             form.worktime_from.data,
